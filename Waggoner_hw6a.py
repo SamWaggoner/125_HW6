e9ef01fe4b11c5522855226e2d271713b9977ebb
Sam Waggoener <79596351+SamWaggoner@users.noreply.github.com>
2021-07-02 14:23:59 -0400

    Update Waggoner_hw6a.py
    Commented out older version

--- Waggoner_hw6a.py
+++ Waggoner_hw6a.py
@@ -58,27 +58,27 @@ print("Okay, glad I could help!")
 # I did not discuss this assignment with anyone other than the course staff. (I
 # got help from Matt.)
 
-def main():
+# def main():
 
-    numlist = []
-    freqlist = []
-    print("Hello! I will calculate the longest sequence of numbers in a list.")
-    print("Type your list of numbers and then type end.")
-    num = (input("Please enter a number: "))
-    while num!= "end":
-        numlist.append(int(num))
-        num = ((input("Please enter a number: ")))
-    print(numlist)
-    freq = 1
-    currfreq = 1
-    numfreq = 0
-    for i in range(len(numlist)-1):
-        if numlist[i] == numlist[i+1]:
-            currfreq += 1
-        if currfreq > freq:
-            freq = currfreq
-            currfreq = 1
-            numfreq = numlist[i]
-    print("The most frequent number was "+str(numfreq)+" which was printed "+str(freq)+" times.")
+#     numlist = []
+#     freqlist = []
+#     print("Hello! I will calculate the longest sequence of numbers in a list.")
+#     print("Type your list of numbers and then type end.")
+#     num = (input("Please enter a number: "))
+#     while num!= "end":
+#         numlist.append(int(num))
+#         num = ((input("Please enter a number: ")))
+#     print(numlist)
+#     freq = 1
+#     currfreq = 1
+#     numfreq = 0
+#     for i in range(len(numlist)-1):
+#         if numlist[i] == numlist[i+1]:
+#             currfreq += 1
+#         if currfreq > freq:
+#             freq = currfreq
+#             currfreq = 1
+#             numfreq = numlist[i]
+#     print("The most frequent number was "+str(numfreq)+" which was printed "+str(freq)+" times.")
 
-main()
+# main()
